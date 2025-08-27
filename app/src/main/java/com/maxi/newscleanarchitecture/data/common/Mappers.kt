5ae8cee0abd6d4ba8d87d6facfac32439932f492
maxi <arora.anubhav93@gmail.com>
2025-08-27 10:58:39 +0530

    Add mappers
    This commit adds mappers that converts models from dao to entity and entity to domain as lists

---
+++ app/src/main/java/com/maxi/newscleanarchitecture/data/common/Mappers.kt
@@ -0,0 +1,9 @@
+package com.maxi.newscleanarchitecture.data.common
+
+import com.maxi.newscleanarchitecture.data.local.entity.ArticleEntity
+import com.maxi.newscleanarchitecture.data.local.entity.toDomain
+import com.maxi.newscleanarchitecture.data.remote.model.ArticleDto
+import com.maxi.newscleanarchitecture.domain.model.Article
+
+fun List<ArticleDto>.toEntityList(): List<ArticleEntity> = map {it.}
+fun List<ArticleEntity>.toDomainList(): List<Article> = map { it.toDomain() }
