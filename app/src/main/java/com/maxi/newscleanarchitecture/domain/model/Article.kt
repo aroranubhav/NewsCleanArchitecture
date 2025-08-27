f5fb4db7d5386c57c79e6acbd64769ce38cb9d80
maxi <arora.anubhav93@gmail.com>
2025-08-27 11:01:48 +0530

    Add domain layer model class

---
+++ app/src/main/java/com/maxi/newscleanarchitecture/domain/model/Article.kt
@@ -0,0 +1,9 @@
+package com.maxi.newscleanarchitecture.domain.model
+
+data class Article(
+    val title: String,
+    val description: String,
+    val url: String,
+    val imageUrl: String,
+   /* val publishedAt: Long?*/
+)
