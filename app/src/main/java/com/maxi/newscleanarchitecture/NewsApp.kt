4ef61974af6fd03f61c9cbdd1203709b91ab030b
maxi <arora.anubhav93@gmail.com>
2025-08-27 18:29:49 +0530

    Add custom workManager initialisation via Hilt

--- app/src/main/java/com/maxi/newscleanarchitecture/NewsApp.kt
+++ app/src/main/java/com/maxi/newscleanarchitecture/NewsApp.kt
@@ -1,7 +1,30 @@
 package com.maxi.newscleanarchitecture
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.WorkManager
+import com.maxi.newscleanarchitecture.worker.NewsSyncScheduler
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NewsApp : Application()
+class NewsApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var workManager: WorkManager
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        NewsSyncScheduler.scheduleNews(workManager)
+    }
+
+}
