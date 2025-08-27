abe899ade2114805c48ad75d899ae4ddaf6fc56a
maxi <arora.anubhav93@gmail.com>
2025-08-27 10:54:01 +0530

    Add common constants between remote and local

---
+++ app/src/main/java/com/maxi/newscleanarchitecture/data/common/DataConstants.kt
@@ -0,0 +1,13 @@
+package com.maxi.newscleanarchitecture.data.common
+
+object DataConstants {
+
+    const val STATUS = "status"
+    const val TOTAL_RESULTS = "total_results"
+    const val ARTICLES = "articles"
+    const val TITLE = "title"
+    const val DESCRIPTION = "description"
+    const val URL = "url"
+    const val IMAGE_URL = "urlToImage"
+    const val PUBLISHED_AT = "publishedAt"
+}
