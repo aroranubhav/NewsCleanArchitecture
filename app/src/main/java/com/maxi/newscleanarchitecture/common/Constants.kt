9b036e0bb076527af03af5926ee22abc9ca36daf
maxi <arora.anubhav93@gmail.com>
2025-08-27 11:05:57 +0530

    Add app level constants

---
+++ app/src/main/java/com/maxi/newscleanarchitecture/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.maxi.newscleanarchitecture.common
+
+object Constants {
+
+    const val NETWORK_ERROR = "A network error occurred!"
+    const val UNKNOWN_ERROR = "Failed to fetch news articles!"
+    const val FIRST_LAUNCH_PREF = "first_launch_pref"
+    const val FIRST_FETCH_DONE = "first_fetch_done"
+}
