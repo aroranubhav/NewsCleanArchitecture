c285d0cb2751c6537517b4549f7f320bf5e2a2d1
maxi <arora.anubhav93@gmail.com>
2025-08-27 18:32:43 +0530

    Update use case provider module

--- app/src/main/java/com/maxi/newscleanarchitecture/di/module/UseCaseModule.kt
+++ app/src/main/java/com/maxi/newscleanarchitecture/di/module/UseCaseModule.kt
@@ -3,6 +3,8 @@ package com.maxi.newscleanarchitecture.di.module
 import com.maxi.newscleanarchitecture.domain.repository.NewsRepository
 import com.maxi.newscleanarchitecture.domain.usecase.news.DefaultNewsUseCase
 import com.maxi.newscleanarchitecture.domain.usecase.news.NewsUseCase
+import com.maxi.newscleanarchitecture.domain.usecase.refreshnews.DefaultRefreshNewsUseCase
+import com.maxi.newscleanarchitecture.domain.usecase.refreshnews.RefreshNewsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,8 +17,15 @@ object UseCaseModule {
 
     @Provides
     @Singleton
-    fun provideUseCase(
+    fun provideNewsUseCase(
         repository: NewsRepository
     ): NewsUseCase =
         DefaultNewsUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideRefreshNewsUseCase(
+        repository: NewsRepository
+    ): RefreshNewsUseCase =
+        DefaultRefreshNewsUseCase(repository)
 }
